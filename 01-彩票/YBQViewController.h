6f7a86a9967f1c0decbe9a42680be045eadad965
严必庆 <yanbiqing@yanbiqingdeMac.local>
2015-05-17 18:50:20 -0700

    Initial Commit

---
+++ 01-彩票/YBQViewController.h
@@ -0,0 +1,13 @@
+//
+//  YBQViewController.h
+//  01-彩票
+//
+//  Created by 严必庆 on 15-5-17.
+//  Copyright (c) 2015年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YBQViewController : UIViewController
+
+@end
---
+++ 01-彩票/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  01-彩票
+//
+//  Created by 严必庆 on 15-5-17.
+//  Copyright (c) 2015年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YBQAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YBQAppDelegate class]));
+    }
+}
